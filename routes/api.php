22aa6d6f2a840d440e46e9026007e264c985ddcc
刘海寒 <meiqian@liuhaihandeMacBook-Air.local>
2019-06-03 22:46:29 +0800

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,17 @@ Route::get('/test', function() {
     return $array;
 })->middleware('cors');
 
-// Route::get('/booklist', 'BooklistController@index')->middleware('client.credentials');
+// 公有路由，无需登录即可访问
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/user', 'API\UsersController@getUser');
+});
 
-// Route::group(['namespace' => 'Frontend\Controllers', 'middleware' => 'cors'], function () {
-//     Route::get('/todayBooks', 'BookController@index');
-//     Route::get('/booklist', 'BooklistController@index')->middleware('client.credentials');
-// });
-// // 公有路由，无需登录即可访问
-// Route::group(['prefix' => 'v1'], function () {
-//     Route::get('/user', 'API\UsersController@getUser');
-// });
+//这里是鉴赏操作的地方，关于鉴赏的增删改查
+Route::group(['prefix' => 'appreciation', 'middleware' => 'auth:api'], function() {
+    Route::post('/', 'AppreciationController@store');
+    Route::post('/{appreciation}', 'AppreciationController@update')->where('appreciation', '/d+');
+});
 
-// //这里是鉴赏操作的地方，关于鉴赏的增删改查
-// Route::group(['prefix' => 'appreciation', 'middleware' => 'auth:api'], function() {
-//     Route::post('/', 'AppreciationController@store');
-//     Route::post('/{appreciation}', 'AppreciationController@update')->where('appreciation', '/d+');
-// });
-
-// Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
-//     //my today target:
-//     //1. 先调通接口，可以传递数据
-//     //2. 完成首页的搭建
-//     //3. 完成所有路由的设定  可以先不实现
-// });
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
+});
 
