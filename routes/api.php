ca76797270341b9f95b75e7854010e406eb0b73d
刘海寒 <meiqian@192.168.0.104>
2019-06-07 23:59:02 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,30 +9,57 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//可跨域
+Route::group(['middleware' => 'cors'], function () {
+    //书籍
+    Route::group(['prefix' => 'book'], function () {
+        //获取首页推荐 书籍
+        Route::get('/todayBooks', 'BookController@index');
+        Route::group(['middleware' => ['auth:api']], function () {
+            // 需要登录以后才能访问的  关于书籍  api
+        });
+        // Route::get('/todayBookLists', 'BooklistController@index');
+    });
+
+    //书单
+    Route::group(['prefix' => 'booklist'], function () {
+        //获取首页推荐 书单
+        Route::get('/todayBookLists', 'BooklistController@index');
+        Route::group(['middleware' => ['auth:api']], function () {
+            // 需要登录以后才能访问的  关于书单  api
+        });
+    });
+
+    //鉴赏
+    Route::group(['prefix' => 'appreciation'], function () {
+        //获取首页推荐 鉴赏
+        Route::get('/todayAppreciation', 'AppreciationController@index');
+        Route::group(['middleware' => ['auth:api']], function () {
+            // 需要登录以后才能访问的  关于鉴赏  api
+        });
+    });
+
+    //用户
+    Route::group(['prefix' => 'user'], function () {
+        //登录注册改密码什么的都在这里
+        Route::group(['middleware' => ['auth:api']], function () {
+            // 需要登录以后才能访问的  关于用户  api
+        });
+    });
+
+    // 排行
+    Route::get('/rank', 'BookController@rank');//收藏排行
+    Route::get('/category', 'BookController@category');//鉴赏排行 with前三篇收藏最多的鉴赏
+
+    //分类涉及到分页
+    // 分类  某认所有书
+    Route::get('/{category}/books', 'BookController@category');
+    //根据标签筛选
+    Route::get('/{tag}/books', 'BookController@tag');
+    //根据收藏 或者 品鉴 排序
+    Route::get('/{sort}/books', 'BookController@sort');
 });
 
-// 测试
-Route::get('/todayBooks', 'BookController@index');
-Route::get('/test', function() {
-    $array = array('foo', 'bar');
-    return $array;
-})->middleware('cors');
-
-// 公有路由，无需登录即可访问
-Route::group(['prefix' => 'v1'], function () {
-    Route::get('/user', 'API\UsersController@getUser');
-});
-
-//这里是鉴赏操作的地方，关于鉴赏的增删改查
-Route::group(['prefix' => 'appreciation', 'middleware' => 'auth:api'], function() {
-    Route::post('/', 'AppreciationController@store');
-    Route::post('/{appreciation}', 'AppreciationController@update')->where('appreciation', '/d+');
-});
-
-Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
-});
 
